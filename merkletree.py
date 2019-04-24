7623494417d61854d24bb4da78d2239c32dc9bd2
Bex Warner <bexmwarner@gmail.com>
2019-04-23 20:25:19 -0400

    clean up code and add randomization

--- merkletree.py
+++ merkletree.py
@@ -1,10 +1,9 @@
-import hashlib, json
+import hashlib, json, pprint, datetime, merkletools, random
 from collections import OrderedDict
 from keymaker import *
-import datetime
-import merkletools
 from employees import Employee
 
+# Transaction class
 class Transaction:
   def __init__(self, who, what, when):
     self.who = who
@@ -18,52 +17,101 @@ class Transaction:
     return string_byte_data
 
   def sign(self):
-    string_byte_data = self.format_data()
     # sign the data
+    string_byte_data = self.format_data()
     signature = self.who.get('key', '').sign_data(string_byte_data)
     return signature, string_byte_data
 
   def verify(self, sig):
+    # verify the data
     string_byte_data = self.format_data()
     return self.who.get('key', '').verify_data(string_byte_data, sig)
 
+# combine the signature with the data from the transaction
 def sign_transaction(data, signature):
     return data + signature
 
-if __name__ == "__main__":
-  # sign root of merkle tree with company key
-  # print out root hash, tree head signature, timestamp, tree size
-  # make an employee's key (at some point need to check if company or employee and get relvant signature ??)
-
-  employeeA = Employee()
-  transaction = Transaction({'id': employeeA.get_id(), 'key': employeeA.get_key()},
-                             'insert on customer database', datetime.datetime.now())
-  signature, data = transaction.sign()
-  signed_transaction = sign_transaction(data, signature)
-
-  employeeB = Employee()
-  diff_transaction = Transaction({'id': employeeB.get_id(), 'key': employeeB.get_key()},
-                                   'insert on shipping database', datetime.datetime.now())
-  diff_signature, diff_data = diff_transaction.sign()
-  diff_signed_transaction = sign_transaction(diff_data, diff_signature)
-
-  mt = merkletools.MerkleTools()
-  mt.add_leaf(signed_transaction.hex())
-  mt.add_leaf(diff_signed_transaction.hex())
-  mt.make_tree()
-  root = mt.get_merkle_root()
-
-  # verify signature
-  def verify_sig(mt, index, employee, transaction):
-    if (mt.validate_proof(mt.get_proof(index),
-        mt.get_leaf(index), mt.get_merkle_root())):
-      print('valid merkle tree')
-      if (transaction[0].hex() == mt.get_leaf(index) and
-          employee.get_key().verify_data(transaction[1], transaction[2])):
-        print('valid signature')
-        return True
+# verify signature
+def verify_sig(mt, index, employee, transaction):
+  if (mt.validate_proof(mt.get_proof(index),
+      mt.get_leaf(index), mt.get_merkle_root())):
+    print('valid merkle tree')
+    if (transaction[0].hex() == mt.get_leaf(index) and
+        employee.get_key().verify_data(transaction[1], transaction[2])):
+      print('valid signature')
+      return True
+    else:
+      print('invalid signature')
+      return False
+  else:
+    print('invalid merkle tree')
     return False
 
-  verifyA = verify_sig(mt, 0, employeeA, [signed_transaction, data, signature])
-  verifyB = verify_sig(mt, 1, employeeB, [diff_signed_transaction, diff_data, diff_signature])
-  print(verifyA, verifyB)
+if __name__ == "__main__":
+  # instantiate merkle tree
+  mt = merkletools.MerkleTools()
+  # generate company's master key
+  company = MasterKey()
+  # generate several employees to use for testing
+  employeeA = Employee()
+  employeeB = Employee()
+  employeeC = Employee()
+  employeeD = Employee()
+  employeeE = Employee()
+  employeeF = Employee()
+  employeeG = Employee()
+  employeeH = Employee()
+  employeeI = Employee()
+  employeeJ = Employee()
+  employees = [ employeeA, employeeB, employeeC, employeeD, employeeE,
+                employeeF, employeeG, employeeH, employeeI, employeeJ ]
+  # generate several 'database entries' to use for testing
+  db_entries = [ 'insert on customer database', 'insert on shipping database',
+                 'delete on customer database', 'delete on shipping database',
+                 'delete on product database', 'insert on product database',
+                 'delete on management database', 'insert on management database',
+                 'delete on payment database', 'insert on payment database' ]
+
+  # create some random transactions
+  for i in range(10):
+    # randomly choose an employee
+    employee = random.choice(employees)
+    # generate transaction (randomly chooses db entry)
+    transaction = Transaction({ 'id': employee.get_id(), 'key': employee.get_key() },
+                               random.choice(db_entries), datetime.datetime.now())
+    # create signature
+    signature, data = transaction.sign()
+    # add signature to transaction
+    signed_transaction = sign_transaction(data, signature)
+    # store combined signature and transaction to merkle tree
+    mt.add_leaf(signed_transaction.hex()) # library requires hex data
+
+    # randomly save the fifth signature to be verified
+    if i == 5:
+      sample_index = 5
+      sample_employee = employee
+      sample_transaction = [ signed_transaction, data, signature ]
+
+  # generate merkle tree
+  mt.make_tree()
+  # get tree size
+  leaves = mt.get_leaf_count()
+  # get tree root
+  root = mt.get_merkle_root()
+  # sign root with company key
+  root_signature = company.sign_data(json.dumps(root, default=str).encode())
+  # aggregate tree data
+  signed_root = { 'root_hash': root, 'tree_head_signature': root_signature,
+                 'timestamp': datetime.datetime.now(), 'tree_size': leaves }
+  pp = pprint.PrettyPrinter(indent=2)
+  print('\n Tree Root Info: \n')
+  pp.pprint(signed_root)
+  print('\n')
+
+  # verify randomly chosen signature
+  verifyTrueSample = verify_sig(mt, sample_index, sample_employee, sample_transaction)
+  print('Verify True Sample: ', verifyTrueSample, '\n')
+
+  # use the same data to verify a different index to demonstrate failure
+  verifyFalseSample = verify_sig(mt, sample_index + 1, sample_employee, sample_transaction)
+  print('Verify False Sample: ', verifyFalseSample)

de9ab1614532c61e5cceda66c851fcd54d98d7be
Bex Warner <bexmwarner@gmail.com>
2019-04-23 20:39:18 -0400

    add gitignore and unnecessary files to renamed testing > encryption experiments directory

--- keymaker.py
+++ keymaker.py
@@ -1,3 +1,5 @@
+# Code provided by Professor Devin Greene
+
 import  cryptography.hazmat.primitives.asymmetric.rsa as rsa
 import cryptography.hazmat.primitives.serialization as serial
 import cryptography.hazmat.primitives.asymmetric.padding as padding
@@ -24,25 +26,25 @@ class MasterKey:
         which can be used for hard storage. Note that this
         file is unencrypted, so be careful!"""
 
-        # N.B. This is a byte string. To convert to a string, just do 
+        # N.B. This is a byte string. To convert to a string, just do
         # something like key_pem.decode('ascii').  This will always
         # work since PEM encodings only contain ascii characters
 
         return self.privkey.private_bytes(serial.Encoding.PEM,
                 serial.PrivateFormat.PKCS8,serial.NoEncryption())
-        
+
     def dump_public(self):
         """ Like dump_private, but the public part.  This, of course,
         should be publicly available. """
 
         return self.pubkey.public_bytes(serial.Encoding.PEM,
-                serial.PublicFormat.PKCS1) 
+                serial.PublicFormat.PKCS1)
 
     def sign_data(self,data):
         """ This signs the data with the private key.  We the
         RSASSA-PSS signature scheme (see rfc 3347) since it is recommended
-        by the pros. N.B. that `data' must a byte string. 
-        
+        by the pros. N.B. that `data' must a byte string.
+
         We will use the SHA256 hashing algorithm, and use a salt
         length of 16"""
 
@@ -56,8 +58,8 @@ class MasterKey:
                     padding.PSS(padding.MGF1(SHA256()),mgf_salt_length),
                     SHA256())
         except InvalidSignature:
-            # Bex and Rose decide how to handle an invalid signature
-            print("Sorry, bad signature")
+            # Invalid signature
+            print("Invalid signature")
         else:
             # Signature was valid
             return True
